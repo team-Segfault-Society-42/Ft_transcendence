1a651891690fdb014f4da6ed2a67370e27e64215
nryser <nryser@student.42lausanne.ch>
2026-03-11 15:53:11 +0100

    refactor(backend): return JSON response from hello endpoint

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello() {
-    return 'Hello from backend Nico!';
+    return {"message":"Hello from backend Nico!"}
   }
 }
