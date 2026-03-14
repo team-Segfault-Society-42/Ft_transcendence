e3970ca6da1943f96d5cbc30fa928963289635d3
simo1616 <simo1616@gmail.com>
2026-03-14 07:57:45 +0100

    feat(game-grid-3x3): onclick #3

--- frontend/src/components/Board.tsx
+++ frontend/src/components/Board.tsx
@@ -1,16 +1,22 @@
 import Square from "./Square"
 
+
+
 export default function Board() {
+	function handleSquareClicked(index: Number) {
+		alert(`Square ${index} clicked`)
+	}
 	return <div className="mt-8 grid grid-cols-3 gap-5">
-		<Square value="1" />
-		<Square value="2" />
-		<Square value="3" />
-		<Square value="4" />
-		<Square value="5" />
-		<Square value="6" />
-		<Square value="7" />
-		<Square value="8" />
-		<Square value="9" />
+		<Square value="" onSquareClick={() => handleSquareClicked(0)} />
+		<Square value="" onSquareClick={() => handleSquareClicked(1)} />
+		<Square value="" onSquareClick={() => handleSquareClicked(2)} />
+		<Square value="" onSquareClick={() => handleSquareClicked(3)} />
+		<Square value="" onSquareClick={() => handleSquareClicked(4)} />
+		<Square value="" onSquareClick={() => handleSquareClicked(5)} />
+		<Square value="" onSquareClick={() => handleSquareClicked(6)} />
+		<Square value="" onSquareClick={() => handleSquareClicked(7)} />
+		<Square value="" onSquareClick={() => handleSquareClicked(8)} />
+
 
 	</div>
 }
--- frontend/src/components/Square.tsx
+++ frontend/src/components/Square.tsx
@@ -1,11 +1,13 @@
 type SquareProps = {
 	value: string
+	onSquareClick: () => void
 }
 
-export default function Square({ value }: SquareProps) {
-	return <main>
-		<button className="h-32 w-32 rounded-xl border border-white bg-fuchsia-400 text-3xl font-bold text-white shadow-md">
-			{value}
-		</button>
-	</main>
+
+
+export default function Square({ value, onSquareClick }: SquareProps) {
+	return <button className="h-32 w-32 rounded-xl border border-white bg-fuchsia-400 text-3xl font-bold text-white shadow-md"
+		onClick={onSquareClick}>
+		{value}
+	</button>
 }
