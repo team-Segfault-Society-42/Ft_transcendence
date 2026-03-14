006181875a5c38d629935f470315e16f2cd49b64
simo1616 <simo1616@gmail.com>
2026-03-14 09:03:14 +0100

    feat(game-grid-3x3): add X en O #4

--- frontend/src/components/Board.tsx
+++ frontend/src/components/Board.tsx
@@ -1,21 +1,41 @@
+import { useState } from "react"
 import Square from "./Square"
 
 
 
 export default function Board() {
-	function handleSquareClicked(index: Number) {
-		alert(`Square ${index} clicked`)
+
+	const [role, setRole] = useState(false)
+	const [state, setState] = useState(["", "", "", "", "", "", "", "", ""])
+
+	function handleSquareClicked(index: number) {
+
+		const copy: string[] = Array.from(state)
+		if (copy[index] === "") {
+			if (!role) {
+				copy[index] = 'X'
+				setRole(!role)
+			}
+			else {
+				copy[index] = 'O'
+				setRole(!role)
+			}
+			setState(copy)
+		}
 	}
+
+
+
 	return <div className="mt-8 grid grid-cols-3 gap-5">
-		<Square value="" onSquareClick={() => handleSquareClicked(0)} />
-		<Square value="" onSquareClick={() => handleSquareClicked(1)} />
-		<Square value="" onSquareClick={() => handleSquareClicked(2)} />
-		<Square value="" onSquareClick={() => handleSquareClicked(3)} />
-		<Square value="" onSquareClick={() => handleSquareClicked(4)} />
-		<Square value="" onSquareClick={() => handleSquareClicked(5)} />
-		<Square value="" onSquareClick={() => handleSquareClicked(6)} />
-		<Square value="" onSquareClick={() => handleSquareClicked(7)} />
-		<Square value="" onSquareClick={() => handleSquareClicked(8)} />
+		<Square value={state[0]} onSquareClick={() => handleSquareClicked(0)} />
+		<Square value={state[1]} onSquareClick={() => handleSquareClicked(1)} />
+		<Square value={state[2]} onSquareClick={() => handleSquareClicked(2)} />
+		<Square value={state[3]} onSquareClick={() => handleSquareClicked(3)} />
+		<Square value={state[4]} onSquareClick={() => handleSquareClicked(4)} />
+		<Square value={state[5]} onSquareClick={() => handleSquareClicked(5)} />
+		<Square value={state[6]} onSquareClick={() => handleSquareClicked(6)} />
+		<Square value={state[7]} onSquareClick={() => handleSquareClicked(7)} />
+		<Square value={state[8]} onSquareClick={() => handleSquareClicked(8)} />
 
 
 	</div>
