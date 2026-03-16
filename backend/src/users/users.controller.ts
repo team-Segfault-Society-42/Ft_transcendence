0e76df06f842c8d9d756daf63cfceb635c6cd6dd
nryser <nryser@student.42lausanne.ch>
2026-03-16 19:13:33 +0100

    feat(api-backend): add GET /users and GET /users/:id routes

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { UsersService } from './users.service';
 
+// @UseGuards(AuthGuard) // TODO later with AUTH
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+	constructor(private usersService: UsersService) {}
+
+	@Get()
+	getUsers() {
+		return this.usersService.getUsers();
+	}
+
+	@Get(':id')
+	getUser(@Param('id', ParseIntPipe) id: number) {
+		return this.usersService.getUser(Number(id));
+	}
+}
+
+
