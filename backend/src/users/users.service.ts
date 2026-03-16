4f866e4b59079ee1f5199fb16c8d8ea062b84df9
nryser <nryser@student.42lausanne.ch>
2026-03-16 18:16:36 +0100

    feat(api-backend): implement mock users service

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+	private users = [
+		{
+			id : 1,
+			username : "nico_42",
+			wins : 10,
+			losses : 2,
+			avatar: "placeholder.png"
+		}
+	];
+
+	getUsers(){
+		return (this.users);
+	}
+
+	getUser(id:  number){
+		const user = this.users.find(user => user.id === id);
+
+		if(!user) {
+			throw new NotFoundException('User not found');
+		}
+		return (user);
+	}
+}
+
