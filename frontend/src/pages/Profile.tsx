ab80cb0284756e1d6928611a3980760eae517100
Nasim Dahman <nas@MacBook-Air-de-Nasim.local>
2026-03-17 14:21:24 +0100

    controlled inputs for editing

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -23,19 +23,27 @@ export default function Profile() {
     const [data, setData] = useState<infoUsers>(MOCK_USER)
     const totalGames = data.wins + data.losses + data.draws
     const [isEdit, isInEdit] = useState(false)
+    const [userName, setUserName] = useState(data.username)
+    const [bio, setBio] = useState(data.bio)
 
     function handleSave() {
         isInEdit(!isEdit)
+        if (isEdit == true) {
+            setData({... data, username: userName, bio: bio})
+            isInEdit(!isEdit)
+
+        }
+            
     }
  
     return (
     <main>
         <div align="center" > Profile </div>
-        <h1> Username : { isEdit ? <input defaultValue={data.username} /> : data.username } </h1>
+        <h1> Username : { isEdit ? <input value={userName} onChange={e => setUserName(e.target.value)} /> : data.username } </h1>
         <h1> Wins :  {data.wins} </h1>
         <h1> Losses : {data.losses} </h1>
         <h1> Draws : {data.draws} </h1>
-        <h1> Bio : {isEdit ? <input defaultValue={data.bio} /> : data.bio } </h1>
+        <h1> Bio : {isEdit ? <input value={bio} onChange={e => setBio(e.target.value)} /> : data.bio } </h1>
         <h1> Winrate : {((data.wins / totalGames) * 100).toFixed(2)} % </h1>
         <h1> Total Games : {totalGames} </h1>
         <img src={data.avatar} alt="Avatar" />
