f5e6cfe065cd9d4e0111b97bbd548729b27d8e18
Jordan Decarroux <jordandecarroux@hotmail.com>
2026-03-17 11:41:21 +0100

    Install + config react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Game from './pages/Game';
 
 function App() {
 	return (
-		<Router>
+		<div>
+			<Navbar/>
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/game" element={<Game />} />
-
 			</Routes>
-		</Router>
+		</div>
 	);
 }
 
