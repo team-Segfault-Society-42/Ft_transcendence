2cb3381962632b8ffde3aaf91e9caa7992496fc5
Jordan Decarroux <jordandecarroux@hotmail.com>
2026-03-18 10:42:55 +0100

    main layout added with header and footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import MainLayout from './components/layout/MainLayout';
 import Home from './pages/Home';
 import Game from './pages/Game';
 
 function App() {
 	return (
 		<div>
-			<Navbar/>
 			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/game" element={<Game />} />
+				<Route element={<MainLayout />}>
+					<Route path="/" element={<Home />} />
+					<Route path="/game" element={<Game />} />
+				</Route>
 			</Routes>
 		</div>
 	);
