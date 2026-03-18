6a5961692df465c8922bb178f91c9bc739dfe020
Nasim Dahman <nas@mac.home>
2026-03-18 17:18:00 +0100

    Add updateUser() function to update user data

--- frontend/src/services/userServices.ts
+++ frontend/src/services/userServices.ts
@@ -1,26 +1,48 @@
-const address = "http://localhost:5173/api/"
+const url = "http://localhost:1024/api/"
 
 export async function getUser(id) { // here i will make a UUID
 
     try {
-        const response = await fetch(address + "/user/" + id + "/")
+        const response = await fetch(url + "user/" + id + "/")
         if (!response.ok) {
-            throw new Error("User not found") // a definir
+            throw new Error("User not found") // to define
         }
         const res = await response.json()
         console.log(res)
-        return(res)
+        return (res)
 
-    } catch (error: any {
+    } catch (error: any) {
         console.log("Error : " + error.message)
+        throw new Error("User not found")
     }
 }
 
-export async function updateUser(id) {
+export async function updateUser(id, data) { // here i will make a UUID
     try {
-        const response = await fetch(address + "/user/" + id + "/") {
+        const response = await fetch(url + "user/" + id + "/", 
+        {
+                method: 'PATCH',
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
 
+            })
+        if (!response.ok) {
+            throw new Error("Edit not permited") // to define 
         }
+        const res = await response.json()
+        console.log(res)
+        return (res)
+    } catch (error: any) {
+        console.log("Error : " + error.message)
+        throw new Error("Edit not permited")
+
     }
 
+}
+
+export const userService = {
+    getUser,
+    updateUser
 }
