e365912faf65b17fe639060b2476bd7a994f5d8f
simo1616 <simo1616@gmail.com>
2026-03-28 22:50:07 +0100

    feat(game): add checkWinner function

--- backend/src/modules/game/game.logic.ts
+++ backend/src/modules/game/game.logic.ts
@@ -25,3 +25,38 @@ export function initGameState(): GameState {
     winner: null,
   };
 }
+
+export function checkWinner(board: CellValue[][]): PlayerSymbol | null {
+  const size = 3;
+  for (let r = 0; r < size; r++) {
+    if (
+      board[r][0] &&
+      board[r][0] === board[r][1] &&
+      board[r][0] === board[r][2]
+    )
+      return board[r][0];
+  }
+  for (let c = 0; c < size; c++) {
+    if (
+      board[0][c] &&
+      board[0][c] === board[1][c] &&
+      board[0][c] === board[1][c]
+    )
+      return board[0][c];
+  }
+  if (
+    board[0][0] &&
+    board[0][0] === board[1][1] &&
+    board[0][0] === board[2][2]
+  ) {
+    return board[0][0];
+  }
+  if (
+    board[0][2] &&
+    board[0][2] === board[1][1] &&
+    board[0][2] === board[2][0]
+  ) {
+    return board[0][2];
+  }
+  return null;
+}
