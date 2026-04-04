deebdb018d3c2321c31f51b99532d98f355430ca
Nasim Dahman <nas@mac.home>
2026-04-04 11:00:46 +0200

    add class-validator rules for MatchResultDto

---
+++ backend/src/modules/game/dto/match-result.dto.ts
@@ -0,0 +1,28 @@
+import { IsOptional, IsNotEmpty, IsNumber, IsPositive, Min } from 'class-validator';
+
+export class MatchResultDto {
+    @IsNotEmpty()
+    @IsNumber()
+    @IsPositive()
+    player1Id: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    @IsPositive()
+    player2Id: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    @Min(0)
+    scoresP1: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    @Min(0)
+    scoresP2: number;
+
+    @IsOptional()
+    @IsNumber()
+    winnerId?: number;
+
+}
