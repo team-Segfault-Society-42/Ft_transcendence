09dadd7d12e4a1a5b30f3a552f2d63ab1412661e
simo1616 <simo1616@gmail.com>
2026-04-05 02:11:09 +0200

    backend(game): fix play move dto no initializer on game

--- backend/src/modules/game/dto/play-move.dto.ts
+++ backend/src/modules/game/dto/play-move.dto.ts
@@ -3,15 +3,15 @@ import { IsNotEmpty, IsNumber, IsString, Max, Min } from 'class-validator';
 export class PlayMoveDto {
   @IsString()
   @IsNotEmpty()
-  gameId: string;
+  gameId!: string;
 
   @IsNumber()
   @Min(0)
   @Max(2)
-  r: number;
+  r!: number;
 
   @IsNumber()
   @Min(0)
   @Max(2)
-  c: number;
+  c!: number;
 }
