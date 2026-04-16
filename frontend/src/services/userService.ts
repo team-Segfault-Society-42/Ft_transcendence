37c5fb956fce1eb644c6d0c6d1917ac0527f4017
simo1616 <simo1616@gmail.com>
2026-04-16 14:15:09 +0200

    fix/ baseURL axios user

--- frontend/src/services/userService.ts
+++ frontend/src/services/userService.ts
@@ -1,48 +1,48 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:1024/api/",
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    withCredentials: true,
-})
+  baseURL: "/api/",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
 
 export async function userLogout() {
-    const response = await api.post('auth/logout')
-    return response.data
+  const response = await api.post("auth/logout");
+  return response.data;
 }
 
 export async function getMe() {
-    const response = await api.get('auth/me')
-    return response.data
+  const response = await api.get("auth/me");
+  return response.data;
 }
 
 export async function userLogin(data: unknown) {
-    const response = await api.post('auth/login', data)
-    return response.data
+  const response = await api.post("auth/login", data);
+  return response.data;
 }
 
 export async function createUser(data: unknown) {
-    const response = await api.post('auth/register', data)
-    return response.data;
+  const response = await api.post("auth/register", data);
+  return response.data;
 }
 
 export async function getUser(id: unknown) {
-    const response = await api.get('users/' + id)
-    return response.data
+  const response = await api.get("users/" + id);
+  return response.data;
 }
 
 export async function updateUser(id: unknown, data: unknown) {
-    const response = await api.patch('users/' + id, data)
-    return response.data
+  const response = await api.patch("users/" + id, data);
+  return response.data;
 }
 
 export const userService = {
-    getUser,
-    updateUser,
-    createUser,
-    userLogin,
-    getMe,
-    userLogout,
-}
+  getUser,
+  updateUser,
+  createUser,
+  userLogin,
+  getMe,
+  userLogout,
+};
