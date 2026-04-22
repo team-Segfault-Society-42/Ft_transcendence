69e6cd0f4003fbfcffb8eb3d7c34eef5068c1c0a
Nasim Dahman <nas@mac.home>
2026-04-22 12:22:33 +0200

    implement unlock achievement service logic

---
+++ backend/src/modules/game/achievements.service.ts
@@ -0,0 +1,38 @@
+import {
+  Injectable,
+  BadRequestException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma, PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class AchievementsService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async unlockAchievement(
+    userId: number,
+    key: string,
+    tx?: Prisma.TransactionClient,
+  ) {
+    const prisma = tx || this.prismaService;
+
+    const achievement = await prisma.achievement.findUnique({
+      where: { key },
+    });
+
+    if (!achievement) return;
+
+    const alreadyHasIt = await prisma.userAchievement.findUnique({
+      where: {
+        userId_achievementId: { userId, achievementId: achievement.id },
+      },
+    });
+
+    if (!alreadyHasIt) {
+      await prisma.userAchievement.create({
+        data: { userId, achievementId: achievement.id },
+      });
+    }
+  }
+}
