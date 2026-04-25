b914735cd79eebd54b06acfb861d5c15eb5ed353
Mikan95 <andrewmeechan@hotmail.com>
2026-04-26 00:02:32 +0200

    backend(Seeding): seed.ts now adds 20 simulated games to the database
    Games are between all ten dummy users and include wins, draws and forfeits.
    The script also wipes all existing games before adding games to ensure running
    the seed script multiple times does not result in duplicate games.

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -22,26 +22,26 @@ const AVATARS = Array.from(
 
 // ── Define Simulated Games Outcome ──────────────────────────────────────────
 const GAMES = [
-  { p1: 0, p2: 1, scoresP1: 5, scoresP2: 2, winner: 0,    endReason: 'win'     },
-  { p1: 2, p2: 3, scoresP1: 1, scoresP2: 5, winner: 3,    endReason: 'win'     },
-  { p1: 4, p2: 5, scoresP1: 5, scoresP2: 5, winner: null, endReason: 'draw'    },
-  { p1: 6, p2: 7, scoresP1: 0, scoresP2: 5, winner: 7,    endReason: 'win'     },
-  { p1: 8, p2: 9, scoresP1: 5, scoresP2: 4, winner: 8,    endReason: 'win'     },
-  { p1: 0, p2: 3, scoresP1: 3, scoresP2: 5, winner: 3,    endReason: 'win'     },
-  { p1: 1, p2: 4, scoresP1: 5, scoresP2: 1, winner: 1,    endReason: 'win'     },
-  { p1: 2, p2: 5, scoresP1: 5, scoresP2: 0, winner: 2,    endReason: 'forfeit' },
-  { p1: 6, p2: 9, scoresP1: 4, scoresP2: 5, winner: 9,    endReason: 'win'     },
-  { p1: 7, p2: 8, scoresP1: 5, scoresP2: 3, winner: 7,    endReason: 'win'     },
-  { p1: 0, p2: 5, scoresP1: 2, scoresP2: 5, winner: 5,    endReason: 'win'     },
-  { p1: 1, p2: 6, scoresP1: 5, scoresP2: 5, winner: null, endReason: 'draw'    },
-  { p1: 3, p2: 8, scoresP1: 5, scoresP2: 2, winner: 3,    endReason: 'win'     },
-  { p1: 4, p2: 9, scoresP1: 1, scoresP2: 5, winner: 9,    endReason: 'forfeit' },
-  { p1: 2, p2: 7, scoresP1: 5, scoresP2: 3, winner: 2,    endReason: 'win'     },
-  { p1: 0, p2: 9, scoresP1: 5, scoresP2: 0, winner: 0,    endReason: 'win'     },
-  { p1: 1, p2: 8, scoresP1: 3, scoresP2: 5, winner: 8,    endReason: 'win'     },
-  { p1: 5, p2: 6, scoresP1: 5, scoresP2: 4, winner: 5,    endReason: 'win'     },
-  { p1: 3, p2: 7, scoresP1: 5, scoresP2: 5, winner: null, endReason: 'draw'    },
-  { p1: 4, p2: 8, scoresP1: 2, scoresP2: 5, winner: 8,    endReason: 'win'     },
+  { p1: 0, p2: 1, s1: 5, s2: 2, winner: 0,    endReason: 'win'     },
+  { p1: 2, p2: 3, s1: 1, s2: 5, winner: 3,    endReason: 'win'     },
+  { p1: 4, p2: 5, s1: 5, s2: 5, winner: null, endReason: 'draw'    },
+  { p1: 6, p2: 7, s1: 0, s2: 5, winner: 7,    endReason: 'win'     },
+  { p1: 8, p2: 9, s1: 5, s2: 4, winner: 8,    endReason: 'win'     },
+  { p1: 0, p2: 3, s1: 3, s2: 5, winner: 3,    endReason: 'win'     },
+  { p1: 1, p2: 4, s1: 5, s2: 1, winner: 1,    endReason: 'win'     },
+  { p1: 2, p2: 5, s1: 5, s2: 0, winner: 2,    endReason: 'forfeit' },
+  { p1: 6, p2: 9, s1: 4, s2: 5, winner: 9,    endReason: 'win'     },
+  { p1: 7, p2: 8, s1: 5, s2: 3, winner: 7,    endReason: 'win'     },
+  { p1: 0, p2: 5, s1: 2, s2: 5, winner: 5,    endReason: 'win'     },
+  { p1: 1, p2: 6, s1: 5, s2: 5, winner: null, endReason: 'draw'    },
+  { p1: 3, p2: 8, s1: 5, s2: 2, winner: 3,    endReason: 'win'     },
+  { p1: 4, p2: 9, s1: 1, s2: 5, winner: 9,    endReason: 'forfeit' },
+  { p1: 2, p2: 7, s1: 5, s2: 3, winner: 2,    endReason: 'win'     },
+  { p1: 0, p2: 9, s1: 5, s2: 0, winner: 0,    endReason: 'win'     },
+  { p1: 1, p2: 8, s1: 3, s2: 5, winner: 8,    endReason: 'win'     },
+  { p1: 5, p2: 6, s1: 5, s2: 4, winner: 5,    endReason: 'win'     },
+  { p1: 3, p2: 7, s1: 5, s2: 5, winner: null, endReason: 'draw'    },
+  { p1: 4, p2: 8, s1: 2, s2: 5, winner: 8,    endReason: 'win'     },
 ];
 
 // ── Seed Database ───────────────────────────────────────────────────────────
@@ -97,7 +97,7 @@ async function main() {
     },
   });
 
-	// ── Update or Create Users ───────────────────────────────────────────
+  // ── Update or Create Users ───────────────────────────────────────────
   const passwordHash = await bcrypt.hash(PASSWORD, 10);
 
   for (let n = 0; n < DUMMY_COUNT; n++) {
@@ -112,6 +112,34 @@ async function main() {
       },
     });
     console.log(`Seeded dummy${n}`);
+  }
+
+  // ── Fetch all users in ascending order ────────────────────────
+  const users = await prisma.user.findMany({
+    where: { username: { in: Array.from({ length: DUMMY_COUNT}, (_, n) => `dummy${n}`) } },
+    orderBy: { username: 'asc' },
+  });
+
+  // ── Wipe any existing games ────────────────────────────────────
+  await prisma.game.deleteMany({
+    where: { OR: [
+      { player1: { username: { startsWith: 'dummy' } } },
+      { player2: { username: { startsWith: 'dummy' } } },
+    ]},
+  });
+
+  // ── Run Game Simulation ───────────────────────────────────────
+  for (const {p1, p2, winner, endReason, s1, s2} of GAMES) {
+    await prisma.game.create({
+      data: {
+        player1Id: users[p1].id,
+        player2Id: users[p2].id,
+        winnerId: winner !== null ? users[winner].id : null,
+        endReason: endReason,
+        scoresP1: s1,
+        scoresP2: s2,
+      },
+    });
   }
 }
 
