75567a621a79514e1ea3f73128e0ce46a5a2d091
simo1616 <simo1616@gmail.com>
2026-04-26 12:37:14 +0200

    feat(livegames): add api call gameApi

--- frontend/src/services/gameApi.ts
+++ frontend/src/services/gameApi.ts
@@ -1,10 +1,17 @@
 import { api } from "@/services/api";
+import type { LivesGamesResponse } from "@/type/game.types";
 
 export async function createGame() {
   const response = await api.post<{ gameId: string }>("game/create");
   return response.data;
 }
 
+export async function getLiveGames() {
+  const response = await api.get<LivesGamesResponse>("game/liveGames");
+  return response.data;
+}
+
 export const gameApi = {
   createGame,
+  getLiveGames,
 };
