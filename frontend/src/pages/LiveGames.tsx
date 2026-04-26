08a7f55dc465e96e40221a83d04e2809dc42a90a
simo1616 <simo1616@gmail.com>
2026-04-26 14:14:12 +0200

    feat(livegames): call fetch livegame

--- frontend/src/pages/LiveGames.tsx
+++ frontend/src/pages/LiveGames.tsx
@@ -2,9 +2,15 @@ import { Card } from "@/components/ui/Card";
 import { Button } from "@/components/ui/Button";
 import { Avatar } from "@/components/ui/Avatar";
 import { useLiveGamesStore } from "@/Store/liveGamesStore";
+import { useEffect } from "react";
 
 export default function LiveGamesDisplay() {
   const games = useLiveGamesStore((state) => state.games);
+  const fetchGames = useLiveGamesStore((state) => state.fetchGame);
+
+  useEffect(() => {
+    fetchGames();
+  }, [fetchGames]);
   return (
     <div className="p-8 space-y-10">
       <section>
