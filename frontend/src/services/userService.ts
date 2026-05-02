1d1b7949b3993972c5c4fa05b7bea699df16b02a
Nasim Dahman <nas@device-68.home>
2026-05-02 11:09:33 +0200

    add winrate and totalGames sorting options

--- frontend/src/services/userService.ts
+++ frontend/src/services/userService.ts
@@ -72,7 +72,7 @@ export async function getUserHistory(id: number) {
     return response.data
 }
 
-export async function getLeaderboard(sortBy?: "xp" | "wins") {
+export async function getLeaderboard(sortBy?: "xp" | "wins" | "winrate" | "totalgames") {
 
     const response = await api.get("users/leaderboard", { params: sortBy ? { sortBy } : {} })
     return response.data
