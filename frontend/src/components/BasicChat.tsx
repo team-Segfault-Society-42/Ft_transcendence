a46fafe7b643d6068548f5a6c54ce3ece65e90ae
simo1616 <simo1616@gmail.com>
2026-05-05 10:30:59 +0200

    feat(chat): control message input

--- frontend/src/components/BasicChat.tsx
+++ frontend/src/components/BasicChat.tsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import { Button } from "./ui/Button";
 import { Input } from "./ui/Input";
 
 export function BasicChat() {
+  const [content, setContent] = useState("");
+
   return (
     <section>
       <h1>Chat</h1>
       <div></div>
-      <Input placeholder="Message" />
+      <Input
+        value={content}
+        onChange={(event) => setContent(event.target.value)}
+        placeholder="Message"
+      />
+      <p>{content}</p>
       <Button>Send</Button>
     </section>
   );
