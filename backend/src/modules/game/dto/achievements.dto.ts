2f37eddc823020b6bed5c30372c185b57e3e958a
Nasim Dahman <nas@mac.home>
2026-05-07 17:44:14 +0200

    add achievementDto for swagger

---
+++ backend/src/modules/game/dto/achievements.dto.ts
@@ -0,0 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class AchievementDto {
+  @ApiProperty({ example: 'FIRST_WIM' })
+  key: string;
+
+  @ApiProperty({ example: 'Frist Blood' })
+  displayName: string;
+
+  @ApiProperty({ example: 'Win your first game.' })
+  description: string;
+
+  @ApiProperty({ example: 'FIRST_WIN_TROPHY' })
+  iconName: string;
+}
