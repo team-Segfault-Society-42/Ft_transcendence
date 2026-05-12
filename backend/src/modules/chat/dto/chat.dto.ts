e6f87786fe5efef94967f8cb0d46e8679d0c8c88
Nasim Dahman <nas@mac.home>
2026-05-12 13:56:23 +0200

    update chat.dto.ts  for translation

--- backend/src/modules/chat/dto/chat.dto.ts
+++ backend/src/modules/chat/dto/chat.dto.ts
@@ -1,8 +1,8 @@
 import { IsString, MaxLength, MinLength } from 'class-validator';
 
 export class SendChatMessageDto {
-  @IsString()
-  @MinLength(1)
-  @MaxLength(500)
+  @IsString({ message: 'ERR_CHAT_MSG_STRING' })
+  @MinLength(1, { message: 'ERR_CHAT_MSG_EMPTY' })
+  @MaxLength(500, { message: 'ERR_CHAT_MSG_TOO_LONG' })
   content!: string;
 }
