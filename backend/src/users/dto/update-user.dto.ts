43ee6cb7681866cc12ecf1c008677e5090c0baea
nico ryser <nryser@student.42lausanne.ch>
2026-05-20 06:28:46 +0200

    feat(auth): add password update dto and all dto translations for IsSting

--- backend/src/users/dto/update-user.dto.ts
+++ backend/src/users/dto/update-user.dto.ts
@@ -2,7 +2,7 @@ import { IsOptional, IsString, MaxLength, MinLength, Matches } from 'class-valid
 
 export class UpdateUserDto {
 	@IsOptional()
-	@IsString()
+	@IsString({ message: 'ERR_USERNAME_STRING' })
 	@MinLength(3, { message: 'ERR_USER_USERNAME_MIN_LENGTH' })
 	@MaxLength(20, { message: 'ERR_USER_USERNAME_MAX_LENGTH' })
 	@Matches(/^[a-zA-Z0-9_]+$/, {
@@ -11,7 +11,7 @@ export class UpdateUserDto {
 	username?: string;
 
 	@IsOptional()
-	@IsString()
+	@IsString({ message: 'ERR_USER_BIO_STRING' })
 	@MaxLength(180, { message: 'ERR_USER_BIO_MAX_LENGTH' })
 	bio?: string;
 
