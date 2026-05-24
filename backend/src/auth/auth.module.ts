3349deb2371a963de2a7b7d98c408ad1c2b2a306
nico ryser <nryser@student.42lausanne.ch>
2026-05-24 23:16:52 +0200

    refactor(auth): clean module and dto formatting

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,6 +1,7 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module, forwardRef } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { HttpModule } from '@nestjs/axios';
+import { PresenceModule } from '../presence/presence.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -8,20 +9,19 @@ import { JwtAuthGuard } from './jwt-auth.guard';
 import { OAuthController } from './oauth.controller';
 import { OAuthService } from './oauth.service';
 import { TwoFactorService } from './twofa.service';
-import { PresenceModule } from '../presence/presence.module';
 
 @Module({
-  imports: [
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '8h' },
-    }),
-    PrismaModule,
-    HttpModule,
-	forwardRef(() => PresenceModule)
-  ],
-  controllers: [AuthController, OAuthController],
-  providers: [AuthService, OAuthService, TwoFactorService, JwtAuthGuard],
-  exports: [JwtAuthGuard, JwtModule],
+	imports: [
+		JwtModule.register({
+			secret: process.env.JWT_SECRET,
+			signOptions: { expiresIn: '8h' },
+		}),
+		PrismaModule,
+		HttpModule,
+		forwardRef(() => PresenceModule),
+	],
+	controllers: [AuthController, OAuthController],
+	providers: [AuthService, OAuthService, TwoFactorService, JwtAuthGuard],
+	exports: [JwtAuthGuard, JwtModule],
 })
 export class AuthModule {}
